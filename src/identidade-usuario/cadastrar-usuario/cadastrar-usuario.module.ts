26fa9ba3e624ab2999fb66965f9cfd7083f0f6ea
joao coura <joaovitorsouzacoura@gmai.com>
2025-04-04 19:50:11 -0300

    fix: corrige o caminho para o controller

--- src/identidade-usuario/cadastrar-usuario/cadastrar-usuario.module.ts
+++ src/identidade-usuario/cadastrar-usuario/cadastrar-usuario.module.ts
@@ -1,8 +1,8 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Usuario } from "../usuario.entity";
-import { CadastrarUsuarioController } from "./cadastrar-usuario.controller";
 import { CadastrarUsuarioService } from "./cadastrar-usuario.service";
+import { CadastrarUsuarioController } from "./controller/cadastrar-usuario.controller";
 
 @Module({
   imports: [TypeOrmModule.forFeature([Usuario])],
