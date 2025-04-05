c975632d942437f9a520f181a33d2963b93d99a2
joao coura <joaovitorsouzacoura@gmai.com>
2025-04-04 22:26:01 -0300

    feat: utiliza DTO

--- src/gerenciamento-url/encurtar-url/controller/encurtar-url.controller.ts
+++ src/gerenciamento-url/encurtar-url/controller/encurtar-url.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { EncurtarUrlDto } from '../encurtar-url.dto';
 import { EncutarUrlService } from '../encutar-url.service';
 
 @Controller('urls')
@@ -10,9 +11,9 @@ export class EncutarUrlController {
   ) {}
 
   @Post()
-  encutar(@Body('origem') origem: string): { encurtada: string } {
-    const codigo = this.encutarUrlService.encutar(origem);
-    this.encutarUrlService.criar(origem, codigo, [], undefined);
+  encutar(@Body() encurtarUrlDto: EncurtarUrlDto): { encurtada: string } {
+    const codigo = this.encutarUrlService.encutar(encurtarUrlDto.origem);
+    this.encutarUrlService.criar(encurtarUrlDto.origem, codigo, [], undefined);
     
     return { encurtada: `${this.configService.get("BASE_URL")}/${codigo}` };
   }
