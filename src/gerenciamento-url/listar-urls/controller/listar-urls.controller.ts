5a03a3c36a334005ba2aa7922760839a512ef066
joao coura <joaovitorsouzacoura@gmai.com>
2025-04-05 11:33:23 -0300

    feat: lista somente as URLs que pertencem ao usuário autenticado

--- src/gerenciamento-url/listar-urls/controller/listar-urls.controller.ts
+++ src/gerenciamento-url/listar-urls/controller/listar-urls.controller.ts
@@ -1,16 +1,27 @@
-import { Controller, Get, UseGuards } from "@nestjs/common";
+import { Controller, Get, HttpStatus, Request, Res, UseGuards } from "@nestjs/common";
+import { Response } from "express";
 import { VerificarJwtGuard } from "../../../identidade-usuario/autenticar-usuario/guards/verificar-jwt.guard";
+import { UsuarioService } from "../../../identidade-usuario/usuario.service";
 import { UrlService } from "../../url.service";
 
 @Controller("urls")
 export class ListarUrlsController {
   constructor(
     private readonly urlService: UrlService,
+    private readonly usuarioService: UsuarioService,
   ) {}
 
-  @UseGuards(VerificarJwtGuard)
   @Get()
-  listar() {
-    return this.urlService.buscarTodos();
+  @UseGuards(VerificarJwtGuard)
+  async listar(@Request() req: { usuario: { email: string } }, @Res() res: Response) {
+    const usuario = await this.usuarioService.buscarPorEmail(req.usuario.email);
+
+    if (usuario === null) {
+      return res.status(HttpStatus.NOT_FOUND).json({ message: "Usuário não encontrado" });
+    }
+    
+    const urls = await this.urlService.buscarTodos(usuario);
+
+    return res.status(HttpStatus.OK).json(urls);
   }
 }
--- src/gerenciamento-url/listar-urls/listar-urls.module.ts
+++ src/gerenciamento-url/listar-urls/listar-urls.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
 import { AutenticarUsuarioModule } from "src/identidade-usuario/autenticar-usuario/autenticar-usuario.module";
+import { UsuarioModule } from "../../identidade-usuario/usuario.module";
 import { UrlModule } from "../url.module";
 import { ListarUrlsController } from "./controller/listar-urls.controller";
 
 @Module({
   controllers: [ListarUrlsController],
-  imports: [UrlModule, AutenticarUsuarioModule],
+  imports: [UrlModule, AutenticarUsuarioModule, UsuarioModule],
   providers: [],
 })
 export class ListarUrlsModule {}
